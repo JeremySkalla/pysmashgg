ff4863ab64b78d57ab281b6d23385f62b860e233
JeremySkalla <JeremySkalla@users.noreply.github.com>
2021-01-11 21:25:52 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pysmashgg",
+    version="0.1.0",
+    author="Jeremy Skalla",
+    author_email="jthroughs@gmail.com",
+    description="Python Wrapper for smash.gg's GraphQL API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JeremySkalla/SmashGGPythonWrapper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['requests']
+)
